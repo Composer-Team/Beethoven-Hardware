156251ebed629895fd5c6872aa1763e3f57e65ef
Chris Kjellqvist <chriskjellqvist@gmail.com>
2025-09-22 14:03:30 -0400

    once more

--- src/test/scala/systolic/chisel/SystolicArray.scala
+++ src/test/scala/systolic/chisel/SystolicArray.scala
@@ -1,69 +1,69 @@
-package systolic.chisel
+// package systolic.chisel
 
-import chisel3._
-import chisel3.util._
-import beethoven.common.ShiftReg
-import chipsalliance.rocketchip.config.Parameters
-import beethoven.common.ShiftRegEnable
+// import chisel3._
+// import chisel3.util._
+// import beethoven.common.ShiftReg
+// import chipsalliance.rocketchip.config.Parameters
+// import beethoven.common.ShiftRegEnable
 
-class SystolicArray()(implicit p: Parameters) extends Module {
-  val io = IO(new Bundle {
-    val act_in = Input(UInt(128.W))
-    val act_valid = Input(Bool())
-    val act_ready = Output(Bool())
+// class SystolicArray()(implicit p: Parameters) extends Module {
+//   val io = IO(new Bundle {
+//     val act_in = Input(UInt(128.W))
+//     val act_valid = Input(Bool())
+//     val act_ready = Output(Bool())
 
-    val wgt_in = Input(UInt(128.W))
-    val wgt_valid = Input(Bool())
-    val wgt_ready = Output(Bool())
+//     val wgt_in = Input(UInt(128.W))
+//     val wgt_valid = Input(Bool())
+//     val wgt_ready = Output(Bool())
 
-    val accumulator_out = Output(UInt(128.W))
-    val accumulator_out_valid = Output(Bool())
-    val accumulator_out_ready = Input(Bool())
+//     val accumulator_out = Output(UInt(128.W))
+//     val accumulator_out_valid = Output(Bool())
+//     val accumulator_out_ready = Input(Bool())
 
-    val ctrl_start_matmul = Input(Bool())
-    val ctrl_start_ready = Output(Bool())
-    val ctrl_inner_dimension = Input(UInt(20.W))
-  })
-  val PEs = Seq.fill(8, 8)(Module(new ProcessingElement()))
+//     val ctrl_start_matmul = Input(Bool())
+//     val ctrl_start_ready = Output(Bool())
+//     val ctrl_inner_dimension = Input(UInt(20.W))
+//   })
+//   val PEs = Seq.fill(8, 8)(Module(new ProcessingElement()))
 
-  val s_idle :: s_go :: s_drain :: s_shift :: Nil = Enum(4)
-  val state = RegInit(s_idle)
+//   val s_idle :: s_go :: s_drain :: s_shift :: Nil = Enum(4)
+//   val state = RegInit(s_idle)
 
-  val shift_out = state === s_shift
-  io.accumulator_out_valid := state === s_shift
-  io.ctrl_start_ready := state === s_idle
+//   val shift_out = state === s_shift
+//   io.accumulator_out_valid := state === s_shift
+//   io.ctrl_start_ready := state === s_idle
   
-  io.wgt_ready := io.act_valid && io.wgt_valid
-  io.act_ready := io.act_valid && io.wgt_valid
+//   io.wgt_ready := io.act_valid && io.wgt_valid
+//   io.act_ready := io.act_valid && io.wgt_valid
 
-  val inner_dimension_ctr = Reg(UInt(20.W))
-  val n_inner_dimension_ctr = inner_dimension_ctr - 1.U
-  val can_increment_inputs = (io.act_valid && io.wgt_valid) || state === s_drain
-  val 
+//   val inner_dimension_ctr = Reg(UInt(20.W))
+//   val n_inner_dimension_ctr = inner_dimension_ctr - 1.U
+//   val can_increment_inputs = (io.act_valid && io.wgt_valid) || state === s_drain
+//   val 
 
-  (0 until 8) foreach { row =>
-    val act_for_row = io.act_in((row + 1) * 16 - 1, row * 16)
-    (0 until 8) foreach { col => 
-      val wgt_for_col = io.wgt_in((col + 1) * 16 - 1, col * 16)
-      val curr = PEs(row)(col)
-      if (row == 0) {
-        curr.io.wgt := ShiftRegEnable(wgt_for_col, col, can_increment_inputs, clock)
-        curr.io.wgt_valid := ShiftRegEnable(io.wgt_valid, col, can_increment_inputs, clock)
-      } else {
-        val prev = PEs(row-1)(col)
-        curr.io.wgt := prev.io.wgt_out
-        curr.io.wgt_valid := prev.io.wgt_valid_out
-      }
-      if (col == 0) {
-        curr.io.act := ShiftRegEnable(act_for_row, row, can_increment_inputs, clock)
-        curr.io.act_valid := ShiftRegEnable(io.wgt_valid, row, can_increment_inputs, clock)
-        io.accumulator_out := curr.io.accumulator
-        curr.io.accumulator_shift := 
-      } else {
-        val prev = PEs(row)(col - 1)
-        curr.io.act := prev.io.act_out
-        curr.io.act_valid := prev.io.act_valid_out
-      }
-    }
-  }
-}
+//   (0 until 8) foreach { row =>
+//     val act_for_row = io.act_in((row + 1) * 16 - 1, row * 16)
+//     (0 until 8) foreach { col => 
+//       val wgt_for_col = io.wgt_in((col + 1) * 16 - 1, col * 16)
+//       val curr = PEs(row)(col)
+//       if (row == 0) {
+//         curr.io.wgt := ShiftRegEnable(wgt_for_col, col, can_increment_inputs, clock)
+//         curr.io.wgt_valid := ShiftRegEnable(io.wgt_valid, col, can_increment_inputs, clock)
+//       } else {
+//         val prev = PEs(row-1)(col)
+//         curr.io.wgt := prev.io.wgt_out
+//         curr.io.wgt_valid := prev.io.wgt_valid_out
+//       }
+//       if (col == 0) {
+//         curr.io.act := ShiftRegEnable(act_for_row, row, can_increment_inputs, clock)
+//         curr.io.act_valid := ShiftRegEnable(io.wgt_valid, row, can_increment_inputs, clock)
+//         io.accumulator_out := curr.io.accumulator
+//         curr.io.accumulator_shift := 
+//       } else {
+//         val prev = PEs(row)(col - 1)
+//         curr.io.act := prev.io.act_out
+//         curr.io.act_valid := prev.io.act_valid_out
+//       }
+//     }
+//   }
+// }
