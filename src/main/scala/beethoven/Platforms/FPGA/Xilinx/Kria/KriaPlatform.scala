6c4e233d62046680ea44d8f54ae64b454fa5f7b5
Chris Kjellqvist <chriskjellqvist@gmail.com>
2025-10-21 16:10:43 -0400

    [fix] 0.1.3
    correct kria platform that somehow got mucked up.

--- src/main/scala/beethoven/Platforms/FPGA/Xilinx/Kria/KriaPlatform.scala
+++ src/main/scala/beethoven/Platforms/FPGA/Xilinx/Kria/KriaPlatform.scala
@@ -26,7 +26,7 @@ case class KriaPlatform(
   override val frontBusBaseAddress: Long = 0x2000000000L
   override val frontBusAddressNBits: Int = 40
   override val frontBusAddressMask: Long = 0xffffL
-  override val frontBusBeatBytes: Int = 8
+  override val frontBusBeatBytes: Int = 16
   override val frontBusProtocol: FrontBusProtocol = new AXIFrontBusProtocol
 
   override val physicalMemoryBytes: Long = 4L << 30
